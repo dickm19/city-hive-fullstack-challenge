b461aa136d3cba12f7bbe700bbb577985640272f
Mary Dick <maryclairedick@gmail.com>
2025-07-30 17:39:35 -0500

    add signup route for users :

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, defaults: { format: :json }
   resources :messages, defaults: { format: :json }
+  post "api/users/signup" => "users#signup", as: :user_signup
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
