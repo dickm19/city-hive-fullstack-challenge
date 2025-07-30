c88d35f3a24270f168d7afc913831d6883a69cdd
Mary Dick <maryclairedick@gmail.com>
2025-07-30 16:27:48 -0500

    add user service with signup action

---
+++ my-sms-messenger/shared/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../src/app/types';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class UserService {
+  constructor(private _http: HttpClient) { }
+  
+  signup(user: User) {
+    return this._http.post('/api/signup', user)
+      .pipe(map((response: any) => response.json()));
+  }
+}
