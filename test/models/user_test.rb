2d5cb2df329192ff6eb76e2c0b1be8a531bc5270
Mary Dick <maryclairedick@gmail.com>
2025-07-30 15:02:33 -0500

    create specs for Message Model

---
+++ test/models/user_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+
+  setup do
+    @user = create(:user)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @user.valid?
+  end
+
+  test "should not be valid without a username" do
+    @user.username = nil
+    assert_not @user.valid?
+  end
+
+  test "should not be valid without a phone number" do
+    @user.phone_number = nil
+    assert_not @user.valid?
+  end
+end
