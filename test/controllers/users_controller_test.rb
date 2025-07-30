75259329b5d03f2298b92fdd6a447f82999fafba
Mary Dick <maryclairedick@gmail.com>
2025-07-30 14:13:01 -0500

    add specs for users controller

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = create(:user)
+  end
+
+  test "should get index" do
+    get users_url, as: :json
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      post users_url, params: { user: { username: "test_user", password: "password@123", phone_number: "1234567890" } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show user" do
+    get user_url(@user), as: :json
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch user_url(@user), params: { user: { phone_number: '0987654321' } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy user" do
+    assert_difference("User.count", -1) do
+      delete user_url(@user), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
