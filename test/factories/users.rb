fa9e1223876977a1396ffe1795f1899434e4e7d1
Mary Dick <maryclairedick@gmail.com>
2025-07-30 14:12:50 -0500

    add factory for users

---
+++ test/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    # Add fields as needed, for example:
+    username { "test_user" }
+    phone_number { "1234567890" }
+    password { "password@123" }
+  end
+end
