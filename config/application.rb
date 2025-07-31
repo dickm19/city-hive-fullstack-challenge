bdf496c9b2128dd07670ce31a68c73dd90f8884c
Mary Dick <maryclairedick@gmail.com>
2025-07-31 10:31:15 -0500

    add session config to application config

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,10 @@ module MySmsMessenger
     config.autoload_lib(ignore: %w[assets tasks])
     
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options   
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
