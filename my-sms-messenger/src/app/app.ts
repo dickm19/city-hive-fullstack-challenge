4e633ff6fd45d48a8fe633002665e89b07f079bc
Mary Dick <maryclairedick@gmail.com>
2025-07-31 12:03:57 -0500

    render sent messages on page load

--- my-sms-messenger/src/app/app.ts
+++ my-sms-messenger/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { User } from './types';
+import { User, Message } from './types';
 import { SignupTemplateComponent } from './forms/signup';
 import { MessageTemplateComponent } from './forms/message';
 import { UserService } from '../../shared/user.service';
+import { MessageService } from '../../shared/message.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,12 +15,18 @@ import { CommonModule } from '@angular/common';
 })
 export class App implements OnInit {
   currentUser: User | null = null;
-  constructor(private _userService: UserService) {}
+  messages: Message[] = [];
+  constructor(private _userService: UserService, private _messageService: MessageService) {}
   protected readonly title = signal('my-sms-messenger');
 
   ngOnInit() {
     this._userService.getCurrentUser().subscribe((response: any) => {
       this.currentUser = response?.user;
+    });
+
+    this._messageService.getMessages().subscribe((response: any) => {
+      this.messages = response;
+      console.log(response);
     });
   }
 }
