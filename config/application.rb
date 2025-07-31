a838e921019bf8645b8af5b340b139e12caa8c23
Mary Dick <maryclairedick@gmail.com>
2025-07-31 11:41:01 -0500

    remove trailing whitespace

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module MySmsMessenger
 
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use config.session_store, config.session_options   
+    config.middleware.use config.session_store, config.session_options
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
