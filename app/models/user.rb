299b2cb0149b87acf50669b0bf607df976784c99
Mary Dick <maryclairedick@gmail.com>
2025-07-31 10:59:39 -0500

    add provider and uid fields to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,21 @@ class User
   field :username, type: String
   field :password_digest, type: String
   field :phone_number, type: String
+  field provider: String
+  field uid: String
 
   validates :username, presence: true, uniqueness: true
   validates :phone_number, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+        user.phone_number = auth['info']['phone_number'] || ""
+      end
+    end
+  end
 end
