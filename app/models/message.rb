9b1893bc7e1a4d30f8345639f56c042d43f819ab
Mary Dick <maryclairedick@gmail.com>
2025-07-31 11:41:22 -0500

    add recipient_number field and validations to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,12 @@
 class Message
   include Mongoid::Document
   include Mongoid::Timestamps
-
+  
   field :content, type: String
+  field :recipient_number, type: String
   belongs_to :user
 
   validates :content, presence: true
+  validates :recipient_number, presence: true, phone: true
   validates :user, presence: true
 end
