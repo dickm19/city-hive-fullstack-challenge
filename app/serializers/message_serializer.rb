1154ee069888244183a10a6e43fc971be904760a
Mary Dick <maryclairedick@gmail.com>
2025-07-31 14:11:13 -0500

    create message serializer with formatted data

---
+++ app/serializers/message_serializer.rb
@@ -0,0 +1,12 @@
+class MessageSerializer < ActiveModel::Serializer
+  attributes :sent_at, :content, :formatted_phone_number
+
+  def sent_at
+    date = object.created_at.utc.strftime("%A, %d-%b-%y %H:%M:%S %Z")
+  end
+
+  def formatted_phone_number
+    phone = Phonelib.parse(object.recipient_number)
+    phone.national
+  end
+end
