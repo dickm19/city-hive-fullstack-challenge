cae6d22529ee2984e47b34d16c47fe382cb44f10
Mary Dick <maryclairedick@gmail.com>
2025-08-02 14:09:56 -0500

    add sent_at, formatted_phone_number, and id to message types and id to user type

--- my-sms-messenger/src/app/types.ts
+++ my-sms-messenger/src/app/types.ts
@@ -2,11 +2,15 @@ interface User {
     username: string;
     password: string;
     phone_number: string;
+    id?: string;
 }
 
 interface Message {
     content: string;
     recipient_number: string;
+    sent_at?: string;
+    formatted_phone_number?: string;
+    id: string;
 }
 
 export type { User, Message };
