1d1fbdfc76f7a1b3a7933ca2480199445f82e229
Mary Dick <maryclairedick@gmail.com>
2025-08-02 14:15:43 -0500

    use full url for sendMessage and getMessages fetch requests

--- my-sms-messenger/shared/message.service.ts
+++ my-sms-messenger/shared/message.service.ts
@@ -8,12 +8,12 @@ export class MessageService {
     constructor(private _http: HttpClient) { }
 
     sendMessage(message: Message) {
-        return this._http.post('api/messages/send', message)
+        return this._http.post('http://localhost:3000/api/messages/send', message , { withCredentials: true })
         .pipe(map((response: any) => response));
     }
 
     getMessages() {
-        return this._http.get('api/messages/index')
+        return this._http.get('http://localhost:3000/api/messages/index', { withCredentials: true })
         .pipe(map((response: any) => response));
     }
 }
