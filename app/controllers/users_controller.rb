f4ad8199ac9b44522f01999933e2a197f083a012
Mary Dick <maryclairedick@gmail.com>
2025-08-04 10:52:33 -0500

    remove unnecessary index action for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,6 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
   wrap_parameters :user, include: [:username, :password, :phone_number]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-    render json: @users, status: :ok
-  end
-
   # GET /users/1
   # GET /users/1.json
   def show
