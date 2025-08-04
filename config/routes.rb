50739de8de400a02e0f0df8659a9ad19842081c1
Mary Dick <maryclairedick@gmail.com>
2025-08-04 12:53:24 -0500

    add missing forward slash to index route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/api/users/current" => "sessions#is_logged_in?"
   post "/login", to: "sessions#login"
   post "/api/messages/send" => "messages#send_message", as: :send_message
+  get "/api/messages/index" => "messages#index", as: :messages
   get "/api/messages/:id" => "messages#show", as: :message
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
