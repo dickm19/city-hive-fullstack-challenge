73d692517e47fc989a615f073dac166c9837f600
Mary Dick <maryclairedick@gmail.com>
2025-08-04 12:52:36 -0500

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   post "/auth/:provider/callback" => "sessions#create"
-  get "/auth/failure" => "sessions#failure"
   get "/signout" => "sessions#destroy", :as => :signout
   get "/api/users/current" => "sessions#is_logged_in?"
   post "/login", to: "sessions#login"
