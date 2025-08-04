0dd1bbfbd2b2387c8707d98f889f74dee211f53c
Mary Dick <maryclairedick@gmail.com>
2025-08-04 12:52:18 -0500

    remove unnecessary user specs

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,11 +5,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = create(:user)
   end
 
-  test "should get index" do
-    get users_url, as: :json
-    assert_response :success
-  end
-
   test "should create user" do
     assert_difference("User.count") do
       post user_signup_url, params: { user: { username: FFaker::Internet.user_name, password: FFaker::Internet.password, phone_number: FFaker::PhoneNumber.short_phone_number } }, as: :json
@@ -23,16 +18,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { phone_number: FFaker::PhoneNumber.short_phone_number, password: FFaker::Internet.password } }, as: :json
-    assert_response :success
-  end
-
-  test "should destroy user" do
-    assert_difference("User.count", -1) do
-      delete user_url(@user), as: :json
-    end
-
-    assert_response :no_content
-  end
 end
