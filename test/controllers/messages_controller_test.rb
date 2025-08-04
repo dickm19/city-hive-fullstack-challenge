d7367461c7176c630b2b2aa0b7f82c2075698fd8
Mary Dick <maryclairedick@gmail.com>
2025-08-04 12:50:19 -0500

    login current user for message specs

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -3,16 +3,18 @@ require "test_helper"
 class MessagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @message = create(:message)
+    @user = create(:user)
+    post login_url, params: { username: @user.username, password: @user.password }, as: :json
   end
-
+  
   test "should get index" do
     get messages_url, as: :json
     assert_response :success
   end
 
-  test "should create message" do
+  test "should send message" do
     assert_difference("Message.count") do
-      post messages_url, params: { message: { content: "Hello, world!", user_id: @message.user_id } }, as: :json
+      post send_message_url, params: { message: { content: "Hello, world!", user_id: @message.user_id, recipient_number: @message.recipient_number } }, as: :json
     end
 
     assert_response :created
