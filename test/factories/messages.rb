adfb01fbc6562516350d6a11e0daffe19668c699
Mary Dick <maryclairedick@gmail.com>
2025-08-04 13:26:25 -0500

    use safe nav for User.first.id

--- test/factories/messages.rb
+++ test/factories/messages.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
     factory :message do
         content { "Fixture message" }
         recipient_number { FFaker::PhoneNumber.short_phone_number }
-        association :user
+        user_id { User.first&.id || create(:user).id }
     end
 end
