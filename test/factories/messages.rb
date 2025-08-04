7bdebc1a1ccfcec2f03e9500f9879991f7b2fbea
Mary Dick <maryclairedick@gmail.com>
2025-08-04 13:22:03 -0500

    use 'association :user' instead of creating a new user for message

--- test/factories/messages.rb
+++ test/factories/messages.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
     factory :message do
         content { "Fixture message" }
         recipient_number { FFaker::PhoneNumber.short_phone_number }
-        user_id { User.first.id || create(:user).id }
+        association :user
     end
 end
